3322d5177a64289e81264ed63c6eacb28de7140e
Petar Penović <pere.kaktus@gmail.com>
2025-01-07 14:55:51 +0100

    Login/Baza

---
+++ src/components/Login.jsx
@@ -0,0 +1,40 @@
+// Login.jsx
+import React, { useState } from "react";
+import { auth } from "../firebaseConfig";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      alert("Prijava uspješna");
+    } catch (error) {
+      console.error("Greška pri prijavi: ", error);
+      alert("Prijava nije uspjela");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/firebaseConfig.js
@@ -0,0 +1,18 @@
+
+// firebaseConfig.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAHL4MVgQV9c1wpeCJzoaWQnmMt_YeuzEc",
+  authDomain: "menza-e1c57.firebaseapp.com",
+  projectId: "menza-e1c57",
+  storageBucket: "menza-e1c57.firebasestorage.app",
+  messagingSenderId: "690344742759",
+  appId: "1:690344742759:web:8564e67113814c8cc969c2"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
