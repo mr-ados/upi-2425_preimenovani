a6af95e95edd17bd386d4a7598ce6b7e6d51673c
Mario Radoš <mrados@pmfst.hr>
2025-01-27 23:15:24 +0100

    Poboljsane lighthouse performanse+izbrisan nepotreban file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import compression from 'vite-plugin-compression';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    compression({
+      verbose: true, // Output to show the compression details
+      disable: false, // Ensure compression is enabled
+      threshold: 10240, // Compress files larger than 10KB
+      algorithm: 'brotli', // Use Brotli (you can also try 'gzip')
+    }),
+  ],
+  build: {
+    // Enable minification in production
+    minify: 'terser', // Terser is the default, but you can explicitly set it
+    terserOptions: {
+      compress: {
+        drop_console: true, // Optional: Remove console logs in production
+      },
+    },
+  },
+});
